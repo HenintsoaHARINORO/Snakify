faca0b0d1d6d1235203271a861f9eea20e8af4cd
HenintsoaHARINORO <48785203+HenintsoaHARINORO@users.noreply.github.com>
2022-10-16 14:04:34 +0300

    Greater than two neighbors

---
+++ Lists/Greater_Than_Neighbors.py
@@ -0,0 +1,12 @@
+input_num = input()
+nums = input_num.split()
+i = 1
+j = 0
+great = []
+while (i+1) < len(nums):
+    if int(nums[i]) > int(nums[i - 1]):
+        if int(nums[i]) > int(nums[i+1]):
+            great.append(int(nums[i]))
+    i += 1
+
+print(len(great))
