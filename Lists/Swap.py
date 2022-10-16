2ca2f738822cdf5795af20cfc71bcc78283e6f1a
HenintsoaHARINORO <48785203+HenintsoaHARINORO@users.noreply.github.com>
2022-10-16 15:14:21 +0300

    Swap neighbors

---
+++ Lists/Swap.py
@@ -0,0 +1,11 @@
+input_num = input()
+nums = input_num.split()
+temp = 0
+i = 1
+j = 0
+while i < len(nums):
+    nums[i - 1], nums[i] = nums[i], nums[i - 1]
+    i += 2
+print(nums)
+for j in range(len(nums)):
+    print(nums[j], sep=" ", end=" ")
